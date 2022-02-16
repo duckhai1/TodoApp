742219cfb49584723bc2a435fb61546980b3f5f9
duckhai1 <huynhduc.khai0@gmail.com>
2022-02-17 00:22:38 +0700

    edit

--- dockerBackend/src/main/java/com/khai/dockerBackend/controller/TodoController.java
+++ dockerBackend/src/main/java/com/khai/dockerBackend/controller/TodoController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin("*")
+@CrossOrigin
 public class TodoController {
 
     private final TodoService todoService;
