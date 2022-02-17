412c7aec582fff9834957df84c3e138fef8d279c
duckhai1 <huynhduc.khai0@gmail.com>
2022-02-17 17:22:37 +0700

    test new backend container

--- dockerBackend/src/main/java/com/khai/dockerBackend/configuration/CorsConfiguration.java
+++ dockerBackend/src/main/java/com/khai/dockerBackend/configuration/CorsConfiguration.java
@@ -6,15 +6,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration
-{
+public class CorsConfiguration {
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
             }
         };
     }
+}
